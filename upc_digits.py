a6c56853d15cc4fc65ddb455d840e1b7df2e4e07
Lyzw57 <askuczynski57@gmail.com>
2019-02-13 20:32:46 +0100

    created list of digits and calculated check sum

--- upc_digits.py
+++ upc_digits.py
@@ -1,13 +1,10 @@
 def check_digits(number: int):
-    digits = []
-    counter = len(str(abs(number))) - 1
+    num_str = str(number)
+    if len(num_str) < 11:
+        num_str = '0' * (11 - len(num_str)) + num_str 
 
-    while counter >= 0:
-        digit = (number // (10 ** counter)) % 10
-        digits.append(digit)
-        counter -= 1
+    digits = [int(digit) for digit in num_str]
 
-    
+    m = ((sum(digits[::2]) * 3) + sum(digits[1::2])) % 10
 
-
-check_digits(12345678)
+    return (10 - m) % 10
